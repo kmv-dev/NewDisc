8e3faa0dbeb0dc1cd27e34621327ba5172888501
kireev <kireevmw@yandex.ru>
2022-10-25 21:15:11 +0700

    add starter

---
+++ src/components/Header/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <div class="header__inner">
+      <img src="../../assets/img/logo.svg" alt="logo">
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 30px 0;
+  &__inner {
+    @include container;
+  }
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    <div class="main__content">
+      <h1>Каталог курсов</h1>
+      <div class="main__search">
+        <input type="text">
+        <button><span class="icon-search"></span></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main {
+  &__content {
+    @include container;
+  }
+}
+</style>
