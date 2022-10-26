40c52c0b0becc78839459f7435a8690ecd535871
kireev <kireevmw@yandex.ru>
2022-10-27 00:15:47 +0700

    in progress

--- src/store/main/mutations.js
+++ src/store/main/mutations.js
@@ -3,5 +3,28 @@ export default {
     state.cards = state.cards.filter(function (item) {
       return item.title?.toLowerCase().includes(searchValue.toLowerCase())
     })
+  },
+  setSortData (state, sortMethod) {
+    if (sortMethod === 'high') {
+      state.cards = state.cards.sort(function (a, b) {
+        return b.cost - a.cost
+      })
+    }
+    if (sortMethod === 'low') {
+      state.cards = state.cards.sort(function (a, b) {
+        return a.cost - b.cost
+      })
+    }
+    if (sortMethod === 'start') {
+      state.cards = state.cards.sort(function (a, b) {
+        return a.title.localeCompare(b.title)
+      })
+    }
+    if (sortMethod === 'end') {
+      const sortArr = state.cards.sort(function (a, b) {
+        return a.title.localeCompare(b.title)
+      })
+      state.cards = sortArr.reverse()
+    }
   }
 }
